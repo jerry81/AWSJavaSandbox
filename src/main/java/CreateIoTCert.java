deb6a39ab1c89f7f6dfacb0d3f189e2db0edba00
Raskolnikov <jerry.tann@endress.com>
2023-08-31 13:46:32 +0800

    🚧 can read file i think, file invalid

--- src/main/java/CreateIoTCert.java
+++ src/main/java/CreateIoTCert.java
@@ -6,15 +6,42 @@ import com.amazonaws.services.iot.model.RegisterCertificateResult;
 import com.amazonaws.regions.AwsRegionProvider;
 import com.amazonaws.regions.DefaultAwsRegionProviderChain;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class CreateIoTCert {
+    private static String readCSRFromFile(String filePath) {
+        try {
+            ClassLoader classLoader = CreateIoTCert.class.getClassLoader();
+            InputStream inputStream = classLoader.getResourceAsStream(filePath);
+            if (inputStream != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                return stringBuilder.toString();
+            } else {
+                System.err.println("CSR resource not found: " + filePath);
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     public static void main(String[] args) {
         // Set up AWS IoT client
         AWSIot awsIotClient = AWSIotClientBuilder.standard().build();
 
         // Read your CSR from a file or another source
-        String csrContents = "<contents of your CSR>";
+        String csrContents = readCSRFromFile("csr_file.csr");
 
-               AwsRegionProvider regionProvider = new DefaultAwsRegionProviderChain();
+        AwsRegionProvider regionProvider = new DefaultAwsRegionProviderChain();
         String region = regionProvider.getRegion();
 
         System.out.println("Configured Region: " + region);
