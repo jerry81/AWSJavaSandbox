0e953135c15f63b3fd994f4f2ea6093982dfae69
Raskolnikov <jerry.tann@endress.com>
2023-09-06 14:22:03 +0800

    🚧 create policy, attempt

--- src/main/java/CreateIoTCert.java
+++ src/main/java/CreateIoTCert.java
@@ -1,10 +1,13 @@
-import com.amazonaws.services.iot.AWSIot;
+/*
+old sdk
+*/
+// import com.amazonaws.services.iot.AWSIot;
 // import com.amazonaws.regions.Region;
-import com.amazonaws.services.iot.AWSIotClientBuilder;
+// import com.amazonaws.services.iot.AWSIotClientBuilder;
 // import com.amazonaws.services.iot.model.RegisterCertificateRequest;
-import com.amazonaws.services.iot.model.RegisterCertificateResult;
-import com.amazonaws.regions.AwsRegionProvider;
-import com.amazonaws.regions.DefaultAwsRegionProviderChain;
+// import com.amazonaws.services.iot.model.RegisterCertificateResult;
+// import com.amazonaws.regions.AwsRegionProvider;
+// import com.amazonaws.regions.DefaultAwsRegionProviderChain;
 // import com.amazonaws.services.iot.model.CreateCertificateFromCsrRequest;
 // import com.amazonaws.services.iot.model.CreateCertificateFromCsrResult;
 
@@ -13,11 +16,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RegExUtils;
+
+
+/* new sdk */
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.iot.IotClient;
 import software.amazon.awssdk.services.iot.model.CreateCertificateFromCsrRequest;
 import software.amazon.awssdk.services.iot.model.CreateCertificateFromCsrResponse;
-
+import software.amazon.awssdk.services.iot.model.GetPolicyRequest;
+import software.amazon.awssdk.services.iot.model.ResourceNotFoundException;
+import software.amazon.awssdk.services.iot.model.CreatePolicyRequest;
 
 public class CreateIoTCert {
     private static String readCSRFromFile(String filePath) {
@@ -42,8 +54,7 @@ public class CreateIoTCert {
         }
     }
 
-    private static void createCert2() {
-      IotClient ic = IotClient.builder().build();
+    private static void createCert2(IotClient ic) {
       CreateCertificateFromCsrRequest request = CreateCertificateFromCsrRequest.builder()
         .certificateSigningRequest(readCSRFromFile("csr.pem"))
         .build();
@@ -78,15 +89,41 @@ public class CreateIoTCert {
     //      System.out.println("Certificate ID: " + result.getCertificateId());
     // }
 
-    private static void createPolicy() {
+    private static void createPolicy(IotClient ic) {
       final String policyName = "test_sensor_TEST-SERIAL";
       final String policyTemplate = "fwr30.json";
+      final GetPolicyRequest request = GetPolicyRequest.builder().policyName(policyName).build();
+      try {
+        ic.getPolicy(request);
+        System.error.println("policy already exists");
+      } catch (final ResourceNotFoundException e) {
+        System.out.println("policy does not exist");
+
+        try (final InputStream input = CreateIoTCert.class.getClassLoader().getResourceAsStream(policyTemplate)) {
+          String template = IOUtils.toString(input, StandardCharsets.UTF_8);
+
+          final Function<String, String> placeholder = key -> "__" + key + "__";
+
+          template = RegExUtils.replaceAll(template, placeholder.apply("serial_number"), "TEST-SERIAL");
+          template = RegExUtils.replaceAll(template, placeholder.apply("IOT_REGION"), "cn-north-1");
+          template = RegExUtils.replaceAll(template, placeholder.apply("AWS_ACCOUNT"), "947302404220");
+          final CreatePolicyRequest policyRequest = CreatePolicyRequest.builder().//
+                    policyName(policyName).//
+                    policyDocument(template).//
+                    build();
+            ic.createPolicy(policyRequest);
+          System.out.println("Created policy " + policyName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+      }
 
     }
 
     public static void main(String[] args) {
         // createCert();
-       createCert2();
-       createPolicy();
+       IotClient ic = IotClient.builder().build();
+       createCert2(ic);
+       createPolicy(ic);
     }
 }
